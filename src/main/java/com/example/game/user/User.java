9462a8c77439b9ea217beaddc37d38fa9a32665e
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-04 00:02:39 +0100

    created user

--- src/main/java/com/example/game/user/User.java
+++ src/main/java/com/example/game/user/User.java
@@ -1,4 +1,28 @@
 package com.example.game.user;
 
+import com.example.game.Citta;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String immagineProfilo;
+    @OneToOne(fetch = FetchType.EAGER)
+    @Column(name = "citta_id")
+    private Citta citta;
 }
