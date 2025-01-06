4d0dbe25a5618d8208054f64fce521b14733fb12
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-06 18:26:25 +0100

    starting to implement methods and logic

--- src/main/java/com/example/game/user/UserRepository.java
+++ src/main/java/com/example/game/user/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.game.user;
 
 import com.example.game.citta.Citta;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -14,8 +13,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByEmailAndIsActive(String email,boolean isActive);
     Page<User> findByCreatedAtDateBetween(LocalDate dateOne, LocalDate dateTwo, Pageable pageable);
-    Page<User> findByNomeContainingIgnoreCase(String nome,Pageable pageable);
-    Page<User> findByCognomeContainingIgnoreCase(String cognome,Pageable pageable);
-    Page<User> findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCase(String nome,String cognome,Pageable pageable);
-    Page<User> findByCitta(Citta citta,Pageable pageable);
+    Page<User> findByNomeContainingIgnoreCaseAndIsActive(String nome,boolean isActive,Pageable pageable);
+    Page<User> findByCognomeContainingIgnoreCaseAndIsActive(String cognome,boolean isActive,Pageable pageable);
+    Page<User> findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCaseAndIsActive(String nome,String cognome,boolean isActive,Pageable pageable);
+    Page<User> findByCittaAndIsActive(Citta citta,boolean isActive,Pageable pageable);
 }
--- src/main/java/com/example/game/user/UserService.java
+++ src/main/java/com/example/game/user/UserService.java
@@ -1,10 +1,29 @@
 package com.example.game.user;
 
+import com.example.game.exceptions.InvalidParamsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+
+
+    public Page<User> findByParamsAndIsActive(String nome, String cognome,int page, int size,String orderBy, String direction){
+        Pageable pageable = PageRequest.of(page,size, Sort.by(Sort.Direction.valueOf(direction),orderBy));
+        if(!"".equals(nome)&&!"".equals(cognome)){
+            return userRepository.findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCaseAndIsActive(nome,cognome,true,pageable);
+        }else if("".equals(nome)&&!"".equals(cognome)){
+            return userRepository.findByCognomeContainingIgnoreCaseAndIsActive(cognome,true,pageable);
+        }else if(!"".equals(nome)){
+            return userRepository.findByNomeContainingIgnoreCaseAndIsActive(nome,true,pageable);
+        }else{
+            throw new InvalidParamsException("I parametri sono vuoti.");
+        }
+    }
 }
