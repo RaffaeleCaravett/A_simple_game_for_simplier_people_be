23a4e2efa4897c75494a140e4ea75f53c341754a
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-08 00:43:56 +0100

    doing user methods

--- src/main/java/com/example/game/user/UserService.java
+++ src/main/java/com/example/game/user/UserService.java
@@ -28,40 +28,40 @@ public class UserService {
     CittaService cittaService;
 
 
-    public Page<User> findByParamsAndIsActive(String nome, String cognome,int page, int size,String orderBy, String direction){
-        Pageable pageable = PageRequest.of(page,size, Sort.by(Sort.Direction.valueOf(direction),orderBy));
-        if(!"".equals(nome)&&!"".equals(cognome)){
-            return userRepository.findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCaseAndIsActive(nome,cognome,true,pageable);
-        }else if("".equals(nome)&&!"".equals(cognome)){
-            return userRepository.findByCognomeContainingIgnoreCaseAndIsActive(cognome,true,pageable);
-        }else if(!"".equals(nome)){
-            return userRepository.findByNomeContainingIgnoreCaseAndIsActive(nome,true,pageable);
-        }else{
+    public Page<User> findByParamsAndIsActive(String nome, String cognome, int page, int size, String orderBy, String direction) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.valueOf(direction), orderBy));
+        if (!"".equals(nome) && !"".equals(cognome)) {
+            return userRepository.findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCaseAndIsActive(nome, cognome, true, pageable);
+        } else if ("".equals(nome) && !"".equals(cognome)) {
+            return userRepository.findByCognomeContainingIgnoreCaseAndIsActive(cognome, true, pageable);
+        } else if (!"".equals(nome)) {
+            return userRepository.findByNomeContainingIgnoreCaseAndIsActive(nome, true, pageable);
+        } else {
             throw new InvalidParamsException("I parametri sono vuoti.");
         }
     }
 
-    public List<User> findByDateBetween(String date1, String date2){
+    public List<User> findByDateBetween(String date1, String date2) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        formatter = formatter.withLocale( Locale.ITALIAN );
+        formatter = formatter.withLocale(Locale.ITALIAN);
         LocalDate from = LocalDate.parse(date1, formatter);
         LocalDate to = LocalDate.parse(date2, formatter);
 
-        return userRepository.findByCreatedAtDateBetween(from,to);
+        return userRepository.findByCreatedAtDateBetween(from, to);
     }
 
-    public Page<User> findByCitta(long id, int page, int size, String orderBy, String direction){
-        Pageable pageable = PageRequest.of(page,size,Sort.by(Sort.Direction.valueOf(direction),orderBy));
+    public Page<User> findByCitta(long id, int page, int size, String orderBy, String direction) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.valueOf(direction), orderBy));
 
         Citta citta = cittaService.findById(id);
 
-        return userRepository.findByCittaAndIsActive(citta,true,pageable);
+        return userRepository.findByCittaAndIsActive(citta, true, pageable);
     }
 
-    public User modifyById(long id, UserSignupDTO userSignupDTO){
+    public User modifyById(long id, UserSignupDTO userSignupDTO) {
         User user = findById(id);
-        if(!user.getEmail().equals(userSignupDTO.email())){
-            if(isEmailUsed(userSignupDTO.email())){
+        if (!user.getEmail().equals(userSignupDTO.email())) {
+            if (isEmailUsed(userSignupDTO.email())) {
                 throw new EmailAlreadyInUseException(userSignupDTO.email());
             }
         }
@@ -70,19 +70,44 @@ public class UserService {
         user.setNome(userSignupDTO.nome());
         user.setEmail(userSignupDTO.email());
         user.setCitta(cittaService.findById(userSignupDTO.cittaId()));
-        
+
         return userRepository.save(user);
     }
 
-    public User findById(long id){
-        return userRepository.findByIdAndIsActive(id,true).orElseThrow(()->new UserWithIdNotFoundException(id));
+    public User findById(long id) {
+        return userRepository.findByIdAndIsActive(id, true).orElseThrow(() -> new UserWithIdNotFoundException(id));
     }
 
-    public User findByEmail(String email){
-        return userRepository.findByEmailAndIsActive(email,true).orElseThrow(()->new UserWithEmailNotFoundException(email));
+    public User findByEmail(String email) {
+        return userRepository.findByEmailAndIsActive(email, true).orElseThrow(() -> new UserWithEmailNotFoundException(email));
     }
 
-    public boolean isEmailUsed(String email){
+    public boolean isEmailUsed(String email) {
         return userRepository.findByEmail(email).isPresent();
+    }
+
+    public boolean deleteById(long id) {
+        User user = findById(id);
+
+        user.setActive(false);
+        return true;
+    }
+
+    public boolean restoreById(long id) {
+        User user = findById(id);
+
+        user.setActive(true);
+        return true;
+    }
+
+    public boolean permanentlyDeleteUser(long id) {
+        User user = findById(id);
+
+        try {
+            userRepository.delete(user);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
