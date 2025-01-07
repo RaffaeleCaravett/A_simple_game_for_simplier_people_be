533fbbd385598a213935216d98c494c551f9e245
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-07 10:29:14 +0100

    doing somethind in user

--- src/main/java/com/example/game/user/UserRepository.java
+++ src/main/java/com/example/game/user/UserRepository.java
@@ -4,15 +4,19 @@ import com.example.game.citta.Citta;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByIdAndIsActive(Long id,boolean isActive);
     Optional<User> findByEmailAndIsActive(String email,boolean isActive);
-    Page<User> findByCreatedAtDateBetween(LocalDate dateOne, LocalDate dateTwo, Pageable pageable);
+    Optional<User> findByEmail(String email);
+    List<User> findByCreatedAtDateBetween(LocalDate dateOne, LocalDate dateTwo);
     Page<User> findByNomeContainingIgnoreCaseAndIsActive(String nome,boolean isActive,Pageable pageable);
     Page<User> findByCognomeContainingIgnoreCaseAndIsActive(String cognome,boolean isActive,Pageable pageable);
     Page<User> findByNomeContainingIgnoreCaseAndCognomeContainingIgnoreCaseAndIsActive(String nome,String cognome,boolean isActive,Pageable pageable);
--- src/main/java/com/example/game/user/UserService.java
+++ src/main/java/com/example/game/user/UserService.java
@@ -1,17 +1,29 @@
 package com.example.game.user;
 
+import com.example.game.citta.Citta;
+import com.example.game.citta.CittaService;
 import com.example.game.exceptions.InvalidParamsException;
+import com.example.game.exceptions.UserWithEmailNotFoundException;
+import com.example.game.payloads.entities.UserSignupDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    CittaService cittaService;
 
 
     public Page<User> findByParamsAndIsActive(String nome, String cognome,int page, int size,String orderBy, String direction){
@@ -25,5 +37,38 @@ public class UserService {
         }else{
             throw new InvalidParamsException("I parametri sono vuoti.");
         }
+    }
+
+    public List<User> findByDateBetween(String date1, String date2){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        formatter = formatter.withLocale( Locale.ITALIAN );
+        LocalDate from = LocalDate.parse(date1, formatter);
+        LocalDate to = LocalDate.parse(date2, formatter);
+
+        return userRepository.findByCreatedAtDateBetween(from,to);
+    }
+
+    public Page<User> findByCitta(long id, int page, int size, String orderBy, String direction){
+        Pageable pageable = PageRequest.of(page,size,Sort.by(Sort.Direction.valueOf(direction),orderBy));
+
+        Citta citta = cittaService.findById(id);
+
+        return userRepository.findByCittaAndIsActive(citta,true,pageable);
+    }
+
+    public User modifyById(long id, UserSignupDTO userSignupDTO){
+
+    }
+
+    public User findById(long id){
+        return userRepository.findByIdAndIsActive(id,true).orElseThrow(()->new UserWithIdNotFoundException(id));
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmailAndIsActive(email,true).orElseThrow(()->new UserWithEmailNotFoundException(email));
+    }
+
+    public boolean isEmailUsed(String email){
+        return userRepository.findByEmail(email).isPresent();
     }
 }
