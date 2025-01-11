b34fe98bf413ec482b2972593100229be481d974
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-11 02:06:34 +0100

    done modify by id

--- src/main/java/com/example/game/user/User.java
+++ src/main/java/com/example/game/user/User.java
@@ -1,13 +1,19 @@
 package com.example.game.user;
 
 import com.example.game.citta.Citta;
+import com.example.game.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,7 +21,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -29,7 +35,38 @@ public class User {
     private LocalDate createdAtDate;
     private String deletedAt;
     private String changePasswordCode;
+    private Role role;
     @ManyToOne(fetch = FetchType.EAGER)
     @Column(name = "citta_id")
     private Citta citta;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/game/user/UserController.java
+++ src/main/java/com/example/game/user/UserController.java
@@ -1,8 +1,14 @@
 package com.example.game.user;
 
+import com.example.game.exceptions.BadRequestException;
 import com.example.game.exceptions.InvalidParamsException;
+import com.example.game.payloads.entities.UserSignupDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -14,6 +20,7 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/byParams")
+    @PreAuthorize("hasAuthority('User')")
     public Page<User> findByParamsAndIsActive(@RequestParam(defaultValue = "") String nome,
                                               @RequestParam(defaultValue = "") String cognome,
                                               @RequestParam(defaultValue = "0") int page,
@@ -28,6 +35,7 @@ public class UserController {
     }
 
     @GetMapping("/byDates")
+    @PreAuthorize("hasAuthority('User')")
     public Page<User> findByDateBetween(@RequestParam(defaultValue = "") String date1,
                                         @RequestParam(defaultValue = "") String date2,
                                         @RequestParam(defaultValue = "0") int page,
@@ -45,5 +53,21 @@ public class UserController {
         }
     }
 
-
+    @GetMapping("/byCitta/{id}")
+    @PreAuthorize("hasAuthority('User')")
+    public Page<User> findByCitta(@PathVariable long id,
+                                  @RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "10") int size,
+                                  @RequestParam(defaultValue = "id") String orderBy,
+                                  @RequestParam(defaultValue = "ASC") String direction){
+        return userService.findByCitta(id,page,size,orderBy,direction);
+    }
+    @PutMapping("")
+    @PreAuthorize("hasAuthority('User')")
+    public User findByCitta(@AuthenticationPrincipal User user, @RequestBody @Validated UserSignupDTO userSignupDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return userService.modifyById(user.getId(),userSignupDTO);
+    }
 }
