97c9b6d47a168e95e43039b30fe274bf03d12ba8
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-01-27 16:58:53 +0100

    git status

--- src/main/java/com/example/game/payloads/errors/ErrorsDTO.java
+++ src/main/java/com/example/game/payloads/errors/ErrorsDTO.java
@@ -1,12 +1,16 @@
 package com.example.game.payloads.errors;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class ErrorsDTO {
     String message;
     Date date;
--- src/main/java/com/example/game/payloads/errors/ErrorsWithListDTO.java
+++ src/main/java/com/example/game/payloads/errors/ErrorsWithListDTO.java
@@ -1,13 +1,17 @@
 package com.example.game.payloads.errors;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class ErrorsWithListDTO {
     String error;
     Date date;
