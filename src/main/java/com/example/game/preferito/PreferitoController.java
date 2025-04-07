5b56808e9a54e68693233f355f5a950beafd6576
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-04-07 23:55:47 +0200

    doing preferito

--- src/main/java/com/example/game/preferito/PreferitoController.java
+++ src/main/java/com/example/game/preferito/PreferitoController.java
@@ -1,9 +1,44 @@
 package com.example.game.preferito;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.game.exceptions.BadRequestException;
+import com.example.game.payloads.entities.PreferitoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/preferito")
+@RequiredArgsConstructor
 public class PreferitoController {
+
+    private final PreferitoService preferitoService;
+
+
+    @GetMapping("/user/{id}")
+    public Page<Preferito> getAllByUserId(@PathVariable long id,
+                                          @RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "10") int size,
+                                          @RequestParam(defaultValue = "id") String orderBy,
+                                          @RequestParam(defaultValue = "ASC") String sortOrder) {
+        return preferitoService.getByUserId(id, page, size, orderBy, sortOrder);
+    }
+
+    @GetMapping("/gioco/{id}")
+    public Page<Preferito> getAllByGiocoId(@PathVariable long id,
+                                          @RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "10") int size,
+                                          @RequestParam(defaultValue = "id") String orderBy,
+                                          @RequestParam(defaultValue = "ASC") String sortOrder){
+        return preferitoService.getByGiocoId(id,page,size,orderBy,sortOrder);
+    }
+
+    @PostMapping("")
+    public Preferito save(@RequestBody @Validated PreferitoDTO preferitoDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return preferitoService.save(preferitoDTO);
+    }
 }
