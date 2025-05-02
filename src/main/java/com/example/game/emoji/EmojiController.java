35a51e4d2e9de1be926fe65622e14cf6c21a1f71
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-05-02 03:11:20 +0200

    learned new thing

--- src/main/java/com/example/game/emoji/EmojiController.java
+++ src/main/java/com/example/game/emoji/EmojiController.java
@@ -1,17 +1,20 @@
 package com.example.game.emoji;
 
+import com.example.game.exceptions.BadRequestException;
+import com.example.game.payloads.entities.EmojiDTO;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/emojies")
 @RequiredArgsConstructor
+@Validated
 public class EmojiController {
 
     private final EmojiService emojiService;
@@ -24,5 +27,12 @@ public class EmojiController {
     @GetMapping("/{title}")
     public List<Emoji> findAllByTitle(@PathVariable String title){
         return emojiService.findAllByTitle(title);
+    }
+    @PostMapping("")
+    public List<Emoji> save(@RequestBody @Validated List<@Valid EmojiDTO> emojiDTOS, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult.getAllErrors());
+        }
+        return emojiService.save(emojiDTOS);
     }
 }
--- src/main/java/com/example/game/emoji/EmojiService.java
+++ src/main/java/com/example/game/emoji/EmojiService.java
@@ -1,5 +1,6 @@
 package com.example.game.emoji;
 
+import com.example.game.payloads.entities.EmojiDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -20,5 +21,16 @@ public class EmojiService {
 
     public List<Emoji> findAllByTitle(String title){
         return emojiRepository.findAllByTitle(title);
+    }
+    public List<Emoji> save(List<EmojiDTO> emojiDTOS){
+        return emojiDTOS.stream().map(this::map).map(emojiRepository::save).toList();
+    }
+
+    private Emoji map(EmojiDTO emojiDTO){
+        return Emoji.builder().
+                emoji(emojiDTO.emoji())
+                .field(emojiDTO.field())
+                .title(emojiDTO.title())
+                .build();
     }
 }
