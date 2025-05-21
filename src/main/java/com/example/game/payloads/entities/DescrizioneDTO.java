777271eb0b34ed8e1187fdb9d4442f8f1d9f12f8
raffaele.caravetta <raffaele.caravetta94@gmail.com>
2025-05-21 16:37:07 +0200

    now doing emojies integration

--- src/main/java/com/example/game/payloads/entities/DescrizioneDTO.java
+++ src/main/java/com/example/game/payloads/entities/DescrizioneDTO.java
@@ -6,7 +6,6 @@ import org.hibernate.validator.constraints.Length;
 
 public record DescrizioneDTO(
         @Length(max = 5000)
-        @NotBlank(message = "Descrizione mancante")
         String innerHTML,
         String textAlignmet
 ) {
